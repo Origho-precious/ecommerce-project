b8941fb687aed53ba021488568228d80f7b9fa78
Origho-precious <orighi8@gmail.com>
2020-07-06 15:49:05 +0100

    fixed html file

---
+++ build/precache-manifest.c0550aca40a2a74badbd5aa249cc6892.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a694c8f454aa588413f1393ac881817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9737051f7be0c82aaf",
+    "url": "/static/css/main.e2a4d012.chunk.css"
+  },
+  {
+    "revision": "c0bc7c1dae81992ec09d",
+    "url": "/static/js/2.3661386d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3661386d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9737051f7be0c82aaf",
+    "url": "/static/js/main.ce079417.chunk.js"
+  },
+  {
+    "revision": "8a8c0a431b44f8966880",
+    "url": "/static/js/runtime-main.4dd908e7.js"
+  },
+  {
+    "revision": "cc82df87c156f2c8ed8229da02f55d89",
+    "url": "/static/media/iphone 11pro.cc82df87.png"
+  },
+  {
+    "revision": "dc7d7a7cf9dd00bc610665013efa04ed",
+    "url": "/static/media/iphone 11promax.dc7d7a7c.png"
+  },
+  {
+    "revision": "a17597f4cda2e17a08f5c2ded2e61960",
+    "url": "/static/media/iphone 6.a17597f4.png"
+  },
+  {
+    "revision": "cf757bea8f41198942db1bb4fe00bae6",
+    "url": "/static/media/iphone 7plus.cf757bea.png"
+  },
+  {
+    "revision": "941860920fd79083f3b901ed4ff174d8",
+    "url": "/static/media/iphone 8.94186092.png"
+  },
+  {
+    "revision": "9fc0c9072483093f12de2c54d8af2039",
+    "url": "/static/media/iphone X.9fc0c907.png"
+  },
+  {
+    "revision": "ce3dfb9728b560f3e818e1e00cf3df41",
+    "url": "/static/media/iphone xmax.ce3dfb97.png"
+  },
+  {
+    "revision": "0684c6ffdcf1a535a27a2cd9bd920c4b",
+    "url": "/static/media/iphone xr.0684c6ff.png"
+  },
+  {
+    "revision": "b1b1202fff857a4c085966219744166b",
+    "url": "/static/media/photo1.b1b1202f.png"
+  },
+  {
+    "revision": "86ef43deffb1fbac059aca6126b27632",
+    "url": "/static/media/photo2.86ef43de.png"
+  },
+  {
+    "revision": "e3b04373b923e8df04f92e2953f919a7",
+    "url": "/static/media/photo3.e3b04373.png"
+  },
+  {
+    "revision": "e6cbacfee60b6267606fe9a47b4f434c",
+    "url": "/static/media/photo4.e6cbacfe.png"
+  },
+  {
+    "revision": "3665645ebc737d2d56b858a2c2dd0c36",
+    "url": "/static/media/photo5.3665645e.png"
+  },
+  {
+    "revision": "6fedb4366ca512697d81cbc2da10c27e",
+    "url": "/static/media/shopping-cart.6fedb436.svg"
+  },
+  {
+    "revision": "5f6eb7caf50edf56c225361e8a8e629d",
+    "url": "/static/media/user.5f6eb7ca.svg"
+  }
+]);
