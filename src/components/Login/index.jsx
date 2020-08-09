dc6610e64bbd0c32fdf3361508825e879f717d95
Origho-precious <orighi8@gmail.com>
2020-08-09 02:56:46 +0100

    rewriting codes

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styles from './login.module.css';
 import { signInWithGoogle } from "../../firebase/firebase";
 
-const Login = ({ setCurrentUser }) => {
-    return (
-      <div className={styles.Login}>
-        <h2>Login to continue to Collections Page</h2>
-        <button onClick={() => {
-            signInWithGoogle();
-                
-        }} type="button">
-          Continue With Google
-        </button>
-      </div>
-    );
+const Login = () => {
+  return (
+    <div className={styles.Login}>
+      <h2>Login to continue to Collections Page</h2>
+      <button onClick={() => signInWithGoogle()} type="button">
+        Continue With Google
+      </button>
+    </div>
+  );
 }
 
-const mapStateToProps = ({ user }) => {
-  return {
-    user,
-  };
-};
-
-export default connect(mapStateToProps)(Login);
+export default Login;
