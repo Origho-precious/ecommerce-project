5bfcf74ac9a0952e47f0592d833ed1207d31f900
Origho-precious <Origho9@gmail.com>
2021-09-28 09:54:29 +0100

    using .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,55 +1,51 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzklmuZsb_5Ait1FatkT-Mg3Vor1hFJ8A",
-    authDomain: "ecomerce-project-40990.firebaseapp.com",
-    databaseURL: "https://ecomerce-project-40990.firebaseio.com",
-    projectId: "ecomerce-project-40990",
-    storageBucket: "ecomerce-project-40990.appspot.com",
-    messagingSenderId: "361284578530",
-    appId: "1:361284578530:web:58896ab84048601b9682bb"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  export const createUserDocument = async (userAuth, additionalData) => {
-    if (!userAuth) {
-      return
-    }
+	apiKey: process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapShot = await userRef.get();
+export const createUserDocument = async (userAuth, additionalData) => {
+	if (!userAuth) {
+		return;
+	}
 
-    if (!snapShot.exists) {
-      const {
-        displayName,
-        email,
-      } = userAuth;
-      const createdAt = new Date();
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
 
-      try {
-        await userRef.set({
-          displayName,
-          email,
-          createdAt,
-          ...additionalData
-        })
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-    return userRef;
-  };
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 
+	return userRef;
+};
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  export default firebase;
+export default firebase;
