af9772a095392fb851be5e7d820a21dc39cd2603
Origho-precious <Origho9@gmail.com>
2021-09-28 10:13:00 +0100

    code refactor

--- src/components/Collection/index.jsx
+++ src/components/Collection/index.jsx
@@ -1,72 +1,69 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import styles from './collections.module.css';
-import Navbar from '../Navbar';
-import { getProducts, addToCart } from '../../actions';
-import STORE_DATA from '../../data/data'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styles from "./collections.module.css";
+import Navbar from "../Navbar";
+import { getProducts, addToCart } from "../../actions";
+import STORE_DATA from "../../data/data";
 
+class Collection extends Component {
+	componentDidMount() {
+		this.props.getProducts(STORE_DATA);
+	}
 
-class Collection extends Component{
-    componentDidMount(){
-        this.props.getProducts(STORE_DATA);
-    }
+	addToCart = (id) => {
+		const product = this.props.products[id];
+		if (this.props.user) {
+			const userId = this.props.user.id;
+			this.props.addToCart(userId, product);
+		}
+	};
 
-    addToCart = id => {
-        const product = this.props.products[id]
-        if (this.props.user) {
-            const userId = this.props.user.id;
-            this.props.addToCart(userId, product);
-        }
-    }
+	renderProducts = () => {
+		if (this.props.products) {
+			return this.props.products.map((product) => {
+				return (
+					<div className={styles.iphone} key={product.id}>
+						<div className={styles.link}>
+							<img src={product.image} alt="icon" />
+							<p className={styles.name}>{product.name}</p>
+							<p className={styles.price}>
+								{product.discountPrice} <span>{product.originalPrice}</span>
+							</p>
+						</div>
+						<button type="button" onClick={() => this.addToCart(product.id)}>
+							Add to Cart
+						</button>
+					</div>
+				);
+			});
+		}
+	};
 
-    renderProducts = () => {
-        if (this.props.products) {
-            return this.props.products.map(product => {
-                return (
-                    <div className={styles.iphone} key={product.id}>
-                        <div className={styles.link}>
-                            <img src={product.image} alt="icon" />
-                            <p className={styles.name}>{product.name}</p>
-                            <p className={styles.price}>
-                                {product.discountPrice}{" "}
-                                <span>{product.originalPrice}</span>
-                            </p>
-                        </div>
-                        <button
-                            type="button"
-                            onClick={() => this.addToCart(product.id)}
-                            >
-                        Add to Cart
-                        </button>
-                  </div>
-                );
-            })
-        }
-    }
-
-    render() {
-        return(
-            <>
-                <Navbar/>
-                <div className={styles.Collection}>
-                    <div className={styles.intro}>
-                        <h1>Iphone Collection</h1>
-                        <p>Discover the collection of fantastic phones from Richard's store, <br/> ranging from mid-range iphone6 to high end devices like iphone 11pro max.</p>
-                    </div>
-                    <div className={styles.grid}>
-                    {this.renderProducts()}
-                    </div>
-                </div>
-            </>
-        )
-    }
-};
+	render() {
+		return (
+			<>
+				<Navbar />
+				<div className={styles.Collection}>
+					<div className={styles.intro}>
+						<h1>Iphone Collection</h1>
+						<p>
+							Discover the collection of fantastic phones from Richard's store,{" "}
+							<br /> ranging from mid-range iphone6 to high end devices like
+							iphone 11pro max.
+						</p>
+					</div>
+					<div className={styles.grid}>{this.renderProducts()}</div>
+				</div>
+			</>
+		);
+	}
+}
 
 const mapStateToProps = ({ user, products }) => {
-    return {
-        user,
-        products
-    }
-}
+	return {
+		user,
+		products,
+	};
+};
 
 export default connect(mapStateToProps, { getProducts, addToCart })(Collection);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import './index.css';
-import App from './components/App';
-import reducers from './reducers';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import "./index.css";
+import App from "./components/App";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}>
-     <App/>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+    <App />
+    Hello
+	</Provider>,
+	document.getElementById("root")
 );
